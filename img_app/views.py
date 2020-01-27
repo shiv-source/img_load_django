2a081567242b25a0fbb40d68c91883433d99e3f4
shiv-source <shivk7466@gmail.com>
2020-01-27 23:11:23 +0530

    img issue fixed

--- img_app/views.py
+++ img_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import UserProfile
 # Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        user = UserProfile(name=name)
+        user.save()
+        return redirect('/')
+    else:
+        data = UserProfile.objects.all()
+        return render(request ,'index.html' , {'data':data})
