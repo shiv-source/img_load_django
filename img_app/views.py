ed8705a74a6ecd6662476835490435b0bffa6028
shiv-source <shivk7466@gmail.com>
2020-01-28 23:28:55 +0530

    image file upload added

--- img_app/views.py
+++ img_app/views.py
@@ -3,11 +3,16 @@ from .models import UserProfile
 # Create your views here.
 
 def home(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        user = UserProfile(name=name)
-        user.save()
-        return redirect('/')
-    else:
+    try:
+        if request.method == 'POST' and request.FILES['img']:
+            name = request.POST['name']
+            img =request.FILES['img']
+            user = UserProfile(name=name,img=img)
+            user.save()
+            return redirect('/')
+        else:
+            data = UserProfile.objects.all()
+            return render(request ,'index.html' , {'data':data})
+    except:
         data = UserProfile.objects.all()
         return render(request ,'index.html' , {'data':data})
