4ec873413f0ecfd34c862e60b47ccdea207ff728
shiv-source <shivk7466@gmail.com>
2020-01-29 02:21:38 -0600

    video upload issue fix

--- img_app/models.py
+++ img_app/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class UserProfile(models.Model):
     name = models.CharField(max_length=50)
-    img = models.ImageField(upload_to='pic/')
+    img = models.FileField(upload_to='pic/')
+    video = models.FileField(upload_to='video/',default=True)
 
     def __str__(self):
         return self.name
--- img_app/views.py
+++ img_app/views.py
@@ -3,16 +3,14 @@ from .models import UserProfile
 # Create your views here.
 
 def home(request):
-    try:
         if request.method == 'POST' and request.FILES['img']:
             name = request.POST['name']
             img =request.FILES['img']
-            user = UserProfile(name=name,img=img)
+            video=request.FILES['video']
+            user = UserProfile(name=name,img=img,video=video)
             user.save()
             return redirect('/')
+        
         else:
             data = UserProfile.objects.all()
             return render(request ,'index.html' , {'data':data})
-    except:
-        data = UserProfile.objects.all()
-        return render(request ,'index.html' , {'data':data})
